cff30ab6a4b8f3fde144be8056e1793e41b81baa
SalvadorICAI <202110911@alu.comillas.edu>
2025-04-14 09:36:53 +0200

    TODO #4

--- p5/src/main/java/edu/comillas/icai/gitt/pat/spring/p5/repository/AppUserRepository.java
+++ p5/src/main/java/edu/comillas/icai/gitt/pat/spring/p5/repository/AppUserRepository.java
@@ -12,4 +12,7 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface AppUserRepository extends CrudRepository<AppUser, Long> {
 
+    AppUser findByEmail(String email);
+
+
 }
